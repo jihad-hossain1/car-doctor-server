880e0b485477ac015aee5a5f2a25ab93b4ec99e6
jihad-hossain1 <jihadkhan934@gmail.com>
2023-05-12 10:30:46 +0600

    json object theke amar j koyta data lagbe specific seykoyta data pawar jonne ay method use korte pari

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -10,8 +10,8 @@ app.use(express.json())
 
 
 app.get('/', (req, res) => {
-    res.send('Car Doctor server is running');
-    
+  res.send('Car Doctor server is running');
+
 })
 
 
@@ -29,16 +29,29 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-      await client.connect();
-      
-      const servicesCollection = client.db('carDoctor').collection('services')
-    
-      app.get('/services', async (req, res) => {
-          const cursor = servicesCollection.find();
-          const result = await cursor.toArray();
-          res.send(result)
+    await client.connect();
+
+    const servicesCollection = client.db('carDoctor').collection('services')
+
+    app.get('/services', async (req, res) => {
+      const cursor = servicesCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
     })
 
+    app.get('/services/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const options = {
+        projection: {
+          title: 1,
+          price: 1,
+          service_id: 1
+        }
+      }
+      const result = await servicesCollection.findOne(query, options);
+      res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -53,5 +66,5 @@ run().catch(console.dir);
 
 
 app.listen(port, () => {
-    console.log(`Car Doctor server run on port ${port}`);
+  console.log(`Car Doctor server run on port ${port}`);
 })
